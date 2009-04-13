a1d8eaa1bbced770347dbf1ca5bf042109c61436
Mat Holroyd <matholroyd@gmail.com>
2009-04-13 22:51:59 +1000

    Initial commit to rails-standard-extensions.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'spec'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'rails_standard_extensions'
+
+Spec::Runner.configure do |config|
+  
+end
